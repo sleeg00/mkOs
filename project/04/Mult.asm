abce65fbe2cf4bd8fca5735f692837f5286c19f0
sleeg00 <slee000220@gmail.com>
2025-04-09 22:34:22 +0900

    [Machine Language] Mult Assmbler

---
+++ project/04/Mult.asm
@@ -0,0 +1,18 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/4/Mult.asm
+
+// Multiplies R0 and R1 and stores the result in R2.
+// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+// The algorithm is based on repetitive addition.
+
+//// Replace this comment with your code.
+@R0
+D=M
+@R1
+D=D+M
+@R2
+M=D
+@END
+0;JMP
